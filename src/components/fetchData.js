15f5ef16449bd2ab277531ca522212665c50fbe0
Artem <renerroll@gmail.com>
2024-10-12 15:12:10 +0300

    base64 keys

--- src/components/fetchData.js
+++ src/components/fetchData.js
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import axios from "axios";
 
-const API_ACCESS_KEY = import.meta.env.VITE_UNSPLASH_ACCESS_KEY;
+const API_ACCESS_KEY = atob(import.meta.env.VITE_UNSPLASH_ACCESS_KEY);
 
 axios.defaults.baseURL = "https://api.unsplash.com/";
 
